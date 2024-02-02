e30b917d159a5f36ac0765fdbc7cfe3c82835db5
trbKnl <ncdeschipper@gmail.com>
2024-02-02 14:43:32 +0100

    removed select username screen

--- src/client/App.vue
+++ src/client/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div id="app">
-    <select-username
-      v-if="!usernameAlreadySelected"
-      @input="onUsernameSelection"
-    />
-    <chat v-else />
+    <chat />
   </div>
 </template>
 
@@ -21,38 +17,35 @@ export default {
   },
   data() {
     return {
-      usernameAlreadySelected: false,
+      sessionsExists: false,
     };
   },
-  methods: {
-    onUsernameSelection(username) {
-      this.usernameAlreadySelected = true;
-      socket.auth = { username };
-      socket.connect();
-    },
-  },
   created() {
+    // Check if session exists in local storage
     const sessionID = localStorage.getItem("sessionID");
-
     if (sessionID) {
-      this.usernameAlreadySelected = true;
+      this.sessionsExists = true;
       socket.auth = { sessionID };
+      socket.connect();
+
+    // If a sessions does not exist request one from the server
+    } else {
+      this.sessionsExists = true;
+      //socket.auth = { username };
       socket.connect();
     }
 
+    // Receive sessionID from server and storge in local storage
     socket.on("session", ({ sessionID, userID, room }) => {
-      // attach the session ID to the next reconnection attempts
       socket.auth = { sessionID };
-      // store it in the localStorage
       localStorage.setItem("sessionID", sessionID);
-      // save the ID of the user
       socket.userID = userID;
       socket.room = room;
     });
 
     socket.on("connect_error", (err) => {
       if (err.message === "invalid username") {
-        this.usernameAlreadySelected = false;
+        this.sessionsExists = false;
       }
     });
   },
