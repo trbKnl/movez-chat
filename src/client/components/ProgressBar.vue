171e0d50c405700d291a475979a81abdaee4393a
bojan2110 <bojan.2110@gmail.com>
2024-06-04 00:09:12 +0200

    Implementing Progress Bar and Waiting Screen

--- src/client/components/ProgressBar.vue
+++ src/client/components/ProgressBar.vue
@@ -1,20 +1,70 @@
 <template>
-    <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-      <div class="bg-blue-600 h-2.5 rounded-full" :style="{ width: value + '%' }"></div>
-    </div>
+	<div class="progress">
+		<div class="bar"></div>
+	</div>
 </template>
-
 
 <script>
 export default {
-  name: "ProgressBar",
-  props: {
-    value: {
-      type: Number,
-      required: true,
-      validator: (value) => value >= 0 && value <= 100,
-    },
-  }
+	name: "ProgressBar",
+	// props: {
+	//   value: {
+	//     type: Number,
+	//     required: true,
+	//     validator: (value) => value >= 0 && value <= 100,
+	//   },
+	// }
 };
 </script>
 
+<style>
+.progress {
+	width: 350px;
+	height: 35px;
+	border-radius: 4px;
+	border: 1px solid #00d2c8;
+	padding: 7px 5px;
+	box-shadow: 0 0 10px #00d2c8;
+}
+
+.progress .bar {
+	/*   width: 0%; */
+	height: 100%;
+	background: repeating-linear-gradient(
+		135deg,
+		#00d2c8,
+		#00d2c8 10px,
+		#fff 10px,
+		#fff 20px
+	);
+	background: 0 0 10px 4px #00d2c8;
+	animation: initial-animation 1s forwards, cyclic-animation 5s infinite 1s;
+}
+
+@keyframes initial-animation {
+	0% {
+		width: 0%;
+	}
+	100% {
+		width: 100%;
+	}
+}
+
+@keyframes cyclic-animation {
+	0% {
+		width: 10%;
+	}
+	25% {
+		width: 100%;
+	}
+	50% {
+		width: 20%;
+	}
+	75% {
+		width: 100%;
+	}
+	100% {
+		width: 100%;
+	}
+}
+</style>
--- src/client/components/Waiting.vue
+++ src/client/components/Waiting.vue
@@ -1,43 +1,41 @@
 <template>
-    <div class="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50">
-    <div class="absolute w-full h-full bg-gray-800 opacity-50"></div>
-      <div class="z-50 text-center">
-          <div class="bg-white p-8 rounded-lg shadow-lg">
-              <img src="/public/images/movez.png" alt="Avatar"  class="mx-auto mb-2 w-16 h-16" />
-        <h2 class="text-2xl font-bold text-gray-800 mb-4">Waiting for {{playersNeeded}} more players <span id="dots" class="ml-1">...</span></h2>
-          </div>
-      </div>
-    </div>
+	<div
+		class="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50"
+	>
+		<div class="absolute w-full h-full bg-white"></div>
+		<div class="z-50 flex flex-col items-center">
+			<h2 class="text-5xl font-thin text-white">
+				Waiting for {{ playersNeeded }} players to start the game...
+			</h2>
+			<ProgressBar />
+		</div>
+	</div>
 </template>
 
 <script>
 import socket from "../socket";
+import ProgressBar from "./ProgressBar.vue";
 
 export default {
-  data() {
-    return {
-      playersNeeded: "3",
-    }
-  },
-  created() {
-    socket.on("update players needed", (playersNeeded) => {
-      this.playersNeeded = playersNeeded
-    })
-  }
-}
-
+	components: {
+		ProgressBar,
+	},
+	data() {
+		return {
+			playersNeeded: "3",
+		};
+	},
+	created() {
+		socket.on("update players needed", (playersNeeded) => {
+			this.playersNeeded = playersNeeded;
+		});
+	},
+};
 </script>
 
 <style scoped>
-  @keyframes pulse {
-      0%, 100% {
-          opacity: 1;
-      }
-      50% {
-          opacity: 0;
-      }
-  }
-  #dots {
-      animation: pulse 1.5s infinite;
-  }
+h2 {
+	color: #6e0069;
+	margin-bottom: 60px;
+}
 </style>
