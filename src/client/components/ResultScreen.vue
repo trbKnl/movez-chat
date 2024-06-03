4cf2ff91d4725dd0e59cd33a9bc4446e2e62f7b0
bojan2110 <bojan.2110@gmail.com>
2024-06-04 00:55:26 +0200

    Result Screen

--- src/client/components/ResultScreen.vue
+++ src/client/components/ResultScreen.vue
@@ -1,63 +1,83 @@
 <template>
-  <div class="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50">
-    <div class="absolute w-full h-full bg-gray-800 opacity-50"></div> 
-    <div class="z-50 text-center">
+	<div v-if="!showThankYou">
+		<div
+			class="fixed top-0 left-0 w-full h-full flex flex-col items-center justify-center z-50"
+		>
+			<div class="absolute w-full h-full bg-gray-800 opacity-50"></div>
+			<div
+				class="z-50 text-center bg-movez-purple w-4/12 h-60 rounded-lg flex items-center justify-center my-7"
+			>
+				<div class="">
+					<div v-if="playerRole === 'Imposter'">
+						<p class="mb-2">Your were the imposter</p>
+					</div>
 
-      <div class="bg-white p-8 rounded-lg shadow-lg">
-        <div v-if="playerRole === 'Imposter'">
-          <p class="text-lg font-semibold text-gray-800 mb-2">Your were the imposter</p>
-        </div>
+					<div v-else>
+						<p class="mb-2">The {{ imposterColor }} user was the imposter</p>
+					</div>
 
-        <div v-else>
-          <p class="text-lg font-semibold text-gray-800 mb-2">The {{ imposterColor}} user was the imposter</p>
-        </div>
+					<p class="mb-2">Player score {{ playerScore }}/3</p>
+					<p class="">Imposter score {{ imposterScore }}/3</p>
 
-        <p class="text-lg font-semibold text-gray-800 mb-2">Player score {{ playerScore }}/3</p>
-        <p class="text-lg font-semibold text-gray-800 mb-2">Imposter score {{ imposterScore }}/3</p>
+					<div v-if="playerWon === true">
+						<p>Congrats on getting that right!</p>
+					</div>
 
-        <div v-if="playerWon === true">
-          <p>Congrats on getting that right!</p>
-        </div>
+					<!-- <div v-if="showThankYou">
+					<thank-you></thank-you>
+				</div> -->
+				</div>
+        <button
+				class="bg-movez-purple text-white p-9 font-bold rounded-lg"
+				@click="handleContinue"
+			  >
+				Continue
+			</button>
 
 
-        <button class="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md m-3" @click="handleContinue">Continue</button>
-        <div v-if="showThankYou">
-          <thank-you ></thank-you>
-        </div>
-    </div>
+			</div>
+
+
+		</div>
+
+
   </div>
-  </div>
+	<div v-if="showThankYou">
+		<thank-you></thank-you>
+	</div>
 </template>
 
 <script>
-import TopicMenu from "./TopicMenu.vue"
-import ThankYou from './ThankYou.vue';
+import TopicMenu from "./TopicMenu.vue";
+import ThankYou from "./ThankYou.vue";
 
 export default {
-  components: {
-    TopicMenu,
-    ThankYou
-  },
-  data() {
-    return {
-      showThankYou: false
-    }
-  },
-  props: [ 
-    "playerScore",
-    "playerRole",
-    "imposterScore",
-    "imposterColor",
-    "playerWon",
-  ],
-  methods: {
-    handleContinue() {
-      this.showThankYou = true
-    }
-  }
-}
+	components: {
+		TopicMenu,
+		ThankYou,
+	},
+	data() {
+		return {
+			showThankYou: false,
+		};
+	},
+	props: [
+		"playerScore",
+		"playerRole",
+		"imposterScore",
+		"imposterColor",
+		"playerWon",
+	],
+	methods: {
+		handleContinue() {
+			this.showThankYou = true;
+		},
+	},
+};
 </script>
 
 <style scoped>
+button {
+	color: white;
+}
 </style>
-
--- src/client/components/ThankYou.vue
+++ src/client/components/ThankYou.vue
@@ -1,44 +1,78 @@
 <template>
-  <div class="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50">
-    <div class="absolute w-full h-full bg-gray-400"></div> 
-    <div class="z-50 text-center">
-      <div class="bg-white p-8 rounded-lg shadow-lg">
+	<div
+		class="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50"
+	>
+		<div class="absolute w-full h-full bg-gray-400"></div>
+		<div class="z-50 text-center">
+			<div class="bg-white">
+				<p class="font-bold text-5xl text-movez-purple my-10 py-10">
+					Thank you for playing Between Us. :-)
+				</p>
+				<p class="font-bold text-5xl text-movez-purple">
+					Now, please fill in the questionnaire below
+				</p>
+				<p class="font-bold text-5xl text-movez-purple">
+					to complete your first task of the program:
+				</p>
+				<button class="bg-movez-purple font-bold rounded-lg mt-10">
+					Start Survey
+				</button>
+				<!-- <img
+					src="/public/images/movez.png"
+					alt="Avatar"
+					class="mx-auto mb-2 w-16 h-16"
+				/>
+				<p class="text-lg font-semibold text-gray-800 mb-2">
+					Thank you for participating in our experiment!
+				</p>
 
-        <img src="/public/images/movez.png" alt="Avatar" class="mx-auto mb-2 w-16 h-16" />
-        <p class="text-lg font-semibold text-gray-800 mb-2">Thank you for participating in our experiment!</p>
+				<div v-if="!isSubmitted">
+					<p class="text-gray-700 mb-4">
+						We hope you enjoyed the game. Please share your suggestions with us.
+					</p>
+					<textarea
+						class="p-3 resize-none w-full border rounded-md focus:outline-none focus:border-blue-500 h-30"
+						placeholder="Your suggestions..."
+					></textarea>
+					<button
+						class="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md m-3"
+						@click="submit"
+					>
+						Submit
+					</button>
+				</div>
 
-        <div v-if="!isSubmitted">
-          <p class="text-gray-700 mb-4">We hope you enjoyed the game. Please share your suggestions with us.</p>
-          <textarea class="p-3 resize-none w-full border rounded-md focus:outline-none focus:border-blue-500 h-30" placeholder="Your suggestions..."></textarea>
-          <button class="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md m-3" @click="submit">Submit</button>
-        </div>
-
-        <div v-else>
-          <p class="text-gray-700">Your feedback has been invaluable.</p>
-        </div>
-
-      </div>
-    </div>
-  </div>
+				<div v-else>
+					<p class="text-gray-700">Your feedback has been invaluable.</p>
+				</div> -->
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 import socket from "../socket";
 
 export default {
-  data() {
-    return {
-      isSubmitted: false, 
-    };
-  },
-  methods: {
-    submit(suggestion) {
-      this.isSubmitted = true
-      socket.emit("suggestion", suggestion)
-    },
-  }
-}
+	data() {
+		return {
+			isSubmitted: false,
+		};
+	},
+	methods: {
+		submit(suggestion) {
+			this.isSubmitted = true;
+			socket.emit("suggestion", suggestion);
+		},
+	},
+};
 </script>
 
 <style scoped>
+button {
+	color: white;
+	width: 300px;
+	font-size: 20px;
+	padding: 7px;
+}
 </style>
